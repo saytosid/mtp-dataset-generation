dcf1c6db1f40309b5754de195066dec74d714cd4
Siddhant Kumar <saytosid@gmail.com>
2017-11-06 15:39:45 +0530

    param update

--- docker-run/gen-ml-files.py
+++ docker-run/gen-ml-files.py
@@ -4,7 +4,7 @@ import numpy as np
 n_samples_factor = 1000000
 for intensity in range(1,6):
 	X,Y,= make_classification(n_samples=intensity*n_samples_factor, 
-	            n_features=51+(intensity), n_informative=45+(intensity), 
+	            n_features=10+(intensity), n_informative=5+(intensity), 
 	            n_repeated=0, n_classes=5)
 
 	np.savetxt(fname='clf_{}_X.gz'.format(intensity),X=X)
@@ -14,7 +14,7 @@ for intensity in range(1,6):
 	# y = np.loadtxt(fname='clf_{}_Y.gz'.format(intensity))
 	
 for intensity in range(1,6):
-	X,Y,= make_regression(n_samples=intensity*n_samples_factor, n_features=51+(intensity), n_informative=45+intensity,
+	X,Y,= make_regression(n_samples=intensity*n_samples_factor, n_features=10+(intensity), n_informative=5+intensity,
 		n_targets=1, bias=0.0, effective_rank=None, tail_strength=0.5, noise=0.01, shuffle=True, coef=False,
 		random_state=None)
 		
