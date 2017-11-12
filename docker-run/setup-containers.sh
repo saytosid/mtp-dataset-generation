3645f602eee405ee6b1d668477265fcbfffbf53e
Siddhant Kumar <saytosid@gmail.com>
2017-11-12 14:00:19 +0530

    updagte

--- docker-run/setup-containers.sh
+++ docker-run/setup-containers.sh
@@ -10,8 +10,20 @@ docker rm cont3
 docker stop cont4
 docker rm cont4
 
+# Kill all tmux sessions
+tmux kill-server
+
 # Run new containers in tmux sessions
 tmux new-session -d -s cont1 'docker run -it -v (pwd):/working_dir --cpus="2" --memory="1000m" --name=cont1 saytosid/myubuntu'
 tmux new-session -d -s cont2 'docker run -it -v (pwd):/working_dir --cpus="2" --memory="1000m" --name=cont2 saytosid/myubuntu'
 tmux new-session -d -s cont3 'docker run -it -v (pwd):/working_dir --cpus="4" --memory="2000m" --name=cont3 saytosid/myubuntu'
 tmux new-session -d -s cont4 'docker run -it -v (pwd):/working_dir --cpus="4" --memory="2000m" --name=cont4 saytosid/myubuntu'
+
+# start data collection
+tmux new-session -d -s collect 'source virtenv/sid/bin/activate.fish'
+tmux -s collect 'python collect-container-metrics.py'
+
+# start stresses
+tmux new-session -d -s stress 'source virtenv/sid/bin/activate.fish'
+tmux -s stress 'python container-stress-all.py'
+
