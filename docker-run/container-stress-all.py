2c253ec0e701fb5d9ff33befc7cc3f4bfada743e
Siddhant Kumar <saytosid@gmail.com>
2017-11-12 15:01:12 +0530

    bug fix

--- docker-run/container-stress-all.py
+++ docker-run/container-stress-all.py
@@ -43,11 +43,15 @@ def start_stressing_on_all_containers(containers):
         time.sleep(LOOP_DELAY)
 
 
+def container_comparator(x,y):
+    return x.name < y.name
+
 if __name__ == '__main__':
     containers = client.containers.list()
+    containers.sort(container_comparator)
     print containers
-    CLUSTER_1 = sorted(containers)[0:len(containers)/2]
-    CLUSTER_2 = sorted(containers)[len(containers)/2:]
+    CLUSTER_1 = containers[0:len(containers)/2]
+    CLUSTER_2 = containers[len(containers)/2:]
     os.system('rm data/container_cluster_info')
     for cont in CLUSTER_1:
         with open('data/container_cluster_info', 'ab') as f:
