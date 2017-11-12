63ff4a5bc24b484b93b896612d9a80eccf5d0c33
Siddhant Kumar <saytosid@gmail.com>
2017-11-12 14:25:38 +0530

    bug fix

--- docker-run/setup-containers.sh
+++ docker-run/setup-containers.sh
@@ -21,17 +21,21 @@ tmux new-session -d -s cont1 'docker run -it -v (pwd):/working_dir --cpus="2" --
 tmux new-session -d -s cont2 'docker run -it -v (pwd):/working_dir --cpus="2" --memory="1000m" --name=cont2 saytosid/myubuntu'
 tmux new-session -d -s cont3 'docker run -it -v (pwd):/working_dir --cpus="4" --memory="2000m" --name=cont3 saytosid/myubuntu'
 tmux new-session -d -s cont4 'docker run -it -v (pwd):/working_dir --cpus="4" --memory="2000m" --name=cont4 saytosid/myubuntu'
+sleep 2
 echo "Containers started in tmux sessions"
 
 # start data collection
 tmux new-session -d -s collect 'fish'
 tmux send -t collect 'source virtenv/sid/bin/activate.fish' ENTER
+tmux send -t collect 'sleep 2' ENTER
 tmux send -t collect 'python collect-container-metrics.py' ENTER
+
 echo "Data collection started"
 
 # start stresses
 tmux new-session -d -s stress 'fish'
 tmux send -t stress 'source virtenv/sid/bin/activate.fish' ENTER
+tmux send -t stress 'sleep 2' ENTER
 tmux send -t stress 'python container-stress-all.py' ENTER
 echo "Stress started"
 
