b99859899fa783aa0ab1a79996611273bce9c014
Siddhant Kumar <saytosid@gmail.com>
2017-11-12 16:43:50 +0530

    bug fix

--- docker-run/gen-ml-files.py
+++ docker-run/gen-ml-files.py
@@ -3,7 +3,7 @@ import numpy as np
 
 n_samples_factor = 1000000
 for intensity in range(1,6):
-	X,Y,= make_classification(n_samples=intensity*n_samples_factor, 
+	X,Y,= make_classification(n_samples=n_samples_factor, 
 	            n_features=10+(intensity), n_informative=5+(intensity), 
 	            n_repeated=0, n_classes=5)
 
@@ -14,7 +14,7 @@ for intensity in range(1,6):
 	# y = np.loadtxt(fname='clf_{}_Y.gz'.format(intensity))
 	
 for intensity in range(1,6):
-	X,Y,= make_regression(n_samples=intensity*n_samples_factor, n_features=10+(intensity), n_informative=5+intensity,
+	X,Y,= make_regression(n_samples=n_samples_factor, n_features=10+(intensity), n_informative=5+intensity,
 		n_targets=1, bias=0.0, effective_rank=None, tail_strength=0.5, noise=0.01, shuffle=True, coef=False,
 		random_state=None)
 		
