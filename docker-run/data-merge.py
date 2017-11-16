dbd4cf0e7d9826c2ff0f7709943e250467febd81
Siddhant Kumar <saytosid@gmail.com>
2017-11-16 12:05:41 +0530

    optimized

--- docker-run/data-merge.py
+++ docker-run/data-merge.py
@@ -4,9 +4,11 @@
 from __future__ import division
 import pandas as pd
 import os
+import re
+SORTED_FILES = {}
 LOAD_AVG_INTERVAL = 1
 def process_file(dir,fname,df):
-    files = sorted(os.listdir(dir))
+    files = SORTED_FILES[cont_name]
     f_idx = [i for i in range(len(files)) if files[i] == fname][0]
     if f_idx > LOAD_AVG_INTERVAL:
         files = files[f_idx-LOAD_AVG_INTERVAL:f_idx+1]
@@ -22,12 +24,12 @@ def process_file(dir,fname,df):
             procs_uninterruptible_index = [i for i in range(len(headers)) if headers[i] == 'container_nr_uninterruptible'][0]
             procs_sleeping_index = [i for i in range(len(headers)) if headers[i] == 'container_nr_sleeping'][0]
             data = lines[-1].split(',')
-            print float(data[procs_running_index]) , float(data[procs_uninterruptible_index]), float(data[procs_sleeping_index])
+            # print float(data[procs_running_index]) , float(data[procs_uninterruptible_index]), float(data[procs_sleeping_index])
             load_ctr += float(data[procs_running_index]) + float(data[procs_uninterruptible_index]) + float(data[procs_sleeping_index])
 
     if len(files) != 0:
         loadavg = load_ctr/len(files)
-        print loadavg
+        # print loadavg
     else:
         loadavg = 0.0
     # print loadavg
@@ -36,16 +38,18 @@ def process_file(dir,fname,df):
 
 if __name__ == '__main__':
     containers = [i for i in os.listdir('data') if not i.endswith('.csv')]
-    containers = [i for i in containers if i.startswith('cont')]
+    containers = [i for i in containers if re.match('^cont\d{1,2}$',i) != None]
     for cont_name in containers:
         print 'Processing files for container - {}'.format(cont_name)
         DF = None
         firsttime = True
-        files = sorted(os.listdir('data/{}'.format(cont_name)))
+        if SORTED_FILES.has_key(cont_name)==False:
+            SORTED_FILES[cont_name] = sorted(os.listdir('data/{}'.format(cont_name)))
+        files = SORTED_FILES[cont_name]
         for filename in files:
             filepath = 'data/{}/{}'.format(cont_name,filename)
             df = pd.read_csv(filepath)
-            df = process_file(dir='data/{}'.format(cont_name),fname=filename,df=df)
+            # df = process_file(dir='data/{}'.format(cont_name),fname=filename,df=df)
             if firsttime:
                 firsttime = False
                 DF = df    
