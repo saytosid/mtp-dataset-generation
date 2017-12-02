55f3888c28cf12af80e1c071f0ba874982747261
Siddhant Kumar <saytosid@gmail.com>
2017-12-02 19:54:00 +0530

    bug fix

--- docker-run/gen-microload-files.py
+++ docker-run/gen-microload-files.py
@@ -3,4 +3,4 @@ from Stress import *
 file_creater = CreateFile()
 
 for intensity in range(1,6):
-	CreateFile.doStress(intensity=intensity,filename='intensity_{}'.format(intensity))
+	file_creater.doStress(intensity=intensity,filename='intensity_{}'.format(intensity))
